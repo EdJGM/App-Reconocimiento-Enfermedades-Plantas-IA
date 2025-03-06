9ccc0f68f8238a647fc5276e0da65a73b82c47f0
Edgar <ejgallegos@espe.edu.ec>
2025-03-06 09:15:04 -0500

    remove unused model and asset files

--- AI-plant-disease-detection-app-main/leafguard/android/app/src/main/kotlin/com/example/leafguard/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.leafguard
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- AI-plant-disease-detection-app-main/leafguard/lib/constants.dart
+++
@@ -1,2 +0,0 @@
-String ip='192.168.1.5'; // your ip address
-String api ='http://$ip';
